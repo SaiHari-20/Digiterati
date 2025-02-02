61fd241be1967dd7b4e98e8e18999e5496c48e9e
SaiHari-20 <esaihari96@gmail.com>
2025-02-02 16:11:36 +0530

    Add files via upload

---
+++ utility/Utility.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class Utility {
+
+	public static int numberLength(int number) {
+		// Static method to find the length of an integer
+		return String.valueOf(Math.abs(number)).length();
+
+	}
+}
