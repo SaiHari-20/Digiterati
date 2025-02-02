19022bdfa7e0095094519613418c9e15e5829afc
unknown <esaihari96@gmail.com>
2025-02-02 15:14:32 +0530

    Initial Files

---
+++ Utility/Utility.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utility;
+
+/**
+ *
+ * @author Sai Hari
+ */
+public class Utility {
+
+    // Static method to find the length of a given integer
+    public static int getLength(int number) {
+        return String.valueOf(number).length();
+    }
+}
